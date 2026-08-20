239d5d8a13686c91fd30b53f893b2a2e466cfff1
yash <yashkokate2000@gmail.com>
2026-08-20 16:55:44 +0530

    complete BidArena auction backend

--- server/src/utils/cookie.utils.js
+++ server/src/utils/cookie.utils.js
@@ -1,14 +1,14 @@
 export let access_cookie_options = {
     httpOnly: true,
-    secure: false,
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 60 * 60 * 1000, // 1 hour
 };
 
 export let refresh_cookie_options = {
     httpOnly: true,
-    secure: false,
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
